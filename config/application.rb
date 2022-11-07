e045c65a680390d2b293a24cc056248bb22da50c
N.Yamaguchi <your.name@example.com>
2022-11-07 19:45:54 +0900

    提出一回目

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module Schedule
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
